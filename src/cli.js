a772eeec1007d5418f5606032afb4c70f8651654
kitovskiy <kitovskiy1@gmail.com>
2024-05-20 18:03:25 +0400

    added brain-even to package.json

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,11 @@
+/* eslint-disable no-console */
 import readLineSync from 'readline-sync';
+
 function greetUser() {
-	const name = readLineSync.question("May I have tour name?");
-	console.log("Hello" + ", " + name + "!");
-};
+  const name = readLineSync.question('May I have your name? ');
+  // eslint-disable-next-line no-useless-concat
+  console.log('Hello' + `, ${name}!`);
+}
 
+// eslint-disable-next-line import/prefer-default-export
 export { greetUser };
-
-
