5deeb41ae74bb0ae2a666925796e8fb50ce2bbd4
kitovskiy <kitovskiy1@gmail.com>
2024-05-27 18:51:11 +0400

    cut from games to src

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readLineSync from 'readline-sync';
+
+const greetUser = () => {
+  const name = readLineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
+  console.log(`Hello, ${name}!`);
+  return name;
+};
+
+export default greetUser;
