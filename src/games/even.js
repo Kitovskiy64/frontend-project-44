eb234db0de7ff7381aa0d30503bdf70c25dd8797
kitovskiy <kitovskiy1@gmail.com>
2024-06-01 09:35:13 +0400

    refactoring

--- src/games/even.js
+++ src/games/even.js
@@ -1,35 +1,7 @@
-import readLineSync from 'readline-sync';
+export const getEvenGameDescription = () => 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEvenNumber = (num) => {
-  if (num % 2 === 0) {
-    return true;
-  }
-  return false;
+export const generateEvenRound = () => {
+  const question = Math.floor(Math.random() * 100) + 1;
+  const correctAnswer = question % 2 === 0 ? 'yes' : 'no';
+  return { question, correctAnswer };
 };
-const evenGame = (name) => {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  let correctAnswersCount = 0;
-
-  while (correctAnswersCount < 3) {
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
-    console.log(`Question: ${randomNumber}`);
-    const userAnswer = readLineSync.question('Your answer: ');
-    let correctAnswer;
-    if (isEvenNumber(randomNumber)) {
-      correctAnswer = 'yes';
-    } else {
-      correctAnswer = 'no';
-    }
-    if ((isEvenNumber(randomNumber) && userAnswer === 'yes') || (!isEvenNumber(randomNumber) && userAnswer === 'no')) {
-      console.log('Correct!');
-      correctAnswersCount += 1;
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${name}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${name}!`);
-};
-
-export default evenGame;
