ece7f0ab1daf5f7239cf889ac9001e67c52ee179
kitovskiy <kitovskiy1@gmail.com>
2024-06-01 09:34:50 +0400

    refactoring

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
 import greetUser from '../src/cli.js';
-import evenGame from '../src/games/even.js';
+import playGame from '../src/engine.js';
+import { getEvenGameDescription, generateEvenRound } from '../src/games/even.js';
 
 const name = greetUser();
+playGame(getEvenGameDescription, generateEvenRound, name);
 
-evenGame(name);
